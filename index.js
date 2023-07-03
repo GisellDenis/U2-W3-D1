491b1095dbcd08791ef1f87e66faccaf2dc4e9b1
GisyWolf96 <gisymoon@gmail.com>
2023-07-03 13:12:58 +0200

    user

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-class Utente{
-    
+class User {
+    constructor(_firstName, _lastName, _age, _location) {
+    this.firstName = _firstName
+    this.lastName = _lastName
+    this.age = _age
+    this.location = _location
+}
 }
